e598e486974d937ab01fb6238875f2bb952ce38d
dafanf <dafa.nurul.tif421@polban.ac.id>
2023-09-23 12:09:22 +0700

    add migration and seeder for users

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            [
+                'nama' => 'Admin',
+                'username' => 'admin',
+                'email' => 'admin@example.com',
+                'email_verified_at' => now(),
+                'password' => Hash::make('admin'),
+                'role' => 'admin',
+                'remember_token' => Str::random(10),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'nama' => 'Himakom',
+                'username' => 'himakom',
+                'email' => 'himakom@example.com',
+                'email_verified_at' => now(),
+                'password' => Hash::make('himakom'),
+                'role' => 'ormawa',
+                'remember_token' => Str::random(10),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'nama' => 'KLI',
+                'username' => 'kli',
+                'email' => 'kli@example.com',
+                'email_verified_at' => now(),
+                'password' => Hash::make('kli'),
+                'role' => 'kli',
+                'remember_token' => Str::random(10),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'nama' => 'Sekumbem',
+                'username' => 'sekumbem',
+                'email' => 'sekumbem@example.com',
+                'email_verified_at' => now(),
+                'password' => Hash::make('sekumbem'),
+                'role' => 'sekumbem',
+                'remember_token' => Str::random(10),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'nama' => 'WD3',
+                'username' => 'wd3',
+                'email' => 'wd3@example.com',
+                'email_verified_at' => now(),
+                'password' => Hash::make('wd3'),
+                'role' => 'wd3',
+                'remember_token' => Str::random(10),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ]);
+    }
+}
