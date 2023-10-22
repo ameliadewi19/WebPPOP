48125526f39b05e50bd23f8871a258d80e98f8e1
fikreynurz <fikreynursyabani@gmail.com>
2023-10-22 19:05:52 +0700

    FIX: TimelineSeeder

--- backend/database/seeders/TimelineSeeder.php
+++ backend/database/seeders/TimelineSeeder.php
@@ -9,21 +9,34 @@ class TimelineSeeder extends Seeder
 {
     public function run()
     {
+        // Data timeline yang ingin Anda masukkan ke database
         $data = [
             [
-                'nama_kegiatan' => 'Kegiatan Pertama',
-                'tanggal_mulai' => '2023-10-20',
-                'tanggal_akhir' => '2023-10-25',
+                'id_kegiatan' => '1',
+                'nama_kegiatan' => 'Kegiatan 1',
+                'tanggal_mulai' => '2023-10-25',
+                'tanggal_selesai' => '2023-10-30',
+                'izin_submit' => 'Izin A',
             ],
             [
-                'nama_kegiatan' => 'Kegiatan Kedua',
+                'id_kegiatan' => '2',
+                'nama_kegiatan' => 'Kegiatan 2',
                 'tanggal_mulai' => '2023-11-05',
-                'tanggal_akhir' => '2023-11-10',
+                'tanggal_selesai' => '2023-11-10',
+                'izin_submit' => 'Izin B',
             ],
+            // Tambahkan data timeline lainnya sesuai kebutuhan Anda
         ];
 
+        // Masukkan data ke dalam tabel 'timeline'
         foreach ($data as $item) {
-            Timeline::create($item);
+            Timeline::create([
+                'id_kegiatan' => $item['id_kegiatan'],
+                'nama_kegiatan' => $item['nama_kegiatan'],
+                'tanggal_mulai' => $item['tanggal_mulai'],
+                'tanggal_selesai' => $item['tanggal_selesai'],
+                'izin_submit' => $item['izin_submit'],
+            ]);
         }
     }
 }
