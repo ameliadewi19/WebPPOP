d4b9f7e2613292fb151b601168afc950615508dc
salsabilamp3 <salsabilamp3@gmail.com>
2023-11-08 13:02:06 +0700

    update front kak dynamic forms

--- frontend/src/pages/Timeline.js
+++ frontend/src/pages/Timeline.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate, useLocation } from 'react-router-dom';
 import feather from 'feather-icons';
-import TambahKegiatanTimelineModal from '../components/Modals/TambahKegiatanTimelineModal';
+import TambahKegiatanTimelineModal from '../components/Modals/TambahKegiatanTimelineModal.js';
 
 const Timeline = () => {
     const [role, setRole] = useState(null);
