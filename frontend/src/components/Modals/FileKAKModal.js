caeb16e263fedc4eb850c9c8b20f78d32c2b2645
salsabilamp3 <salsabilamp3@gmail.com>
2023-11-16 22:59:32 +0700

    fix passing file to show

--- frontend/src/components/Modals/FileKAKModal.js
+++ frontend/src/components/Modals/FileKAKModal.js
@@ -3,11 +3,6 @@ import PDFViewer from './PDFViewer';
 
 const FileKAKModal = ({ pdfData, showModal, setShowModal }) => {
     const modalRef = useRef();
-    console.log("Ini file", pdfData);
-
-    // Pastikan pdfData tersedia sebelum membuat elemen PDFViewer
-    const pdfViewerElement = pdfData ? <PDFViewer pdfUrl={pdfData} /> : null;
-    
     return (
         <div className={'modal fade'} id="FileKAKModal" tabIndex="-1" aria-labelledby="FileKAKModalLabel" aria-hidden={!showModal}>
           <div className="modal-dialog modal-dialog-centered modal-lg">
@@ -17,7 +12,7 @@ const FileKAKModal = ({ pdfData, showModal, setShowModal }) => {
                 <button type="button" className="d-none" ref={modalRef} data-bs-dismiss="modal"></button>
               </div>
               <div className="modal-body">
-                {pdfViewerElement}
+                {pdfData && <PDFViewer pdfUrl={pdfData} />}
               </div>
               <div className="modal-footer">
                 <button type="button" className="btn btn-secondary" data-bs-dismiss="modal" onClick={() => setShowModal(false)}>Tutup</button>
--- frontend/src/components/Modals/PDFViewer.js
+++ frontend/src/components/Modals/PDFViewer.js
@@ -3,23 +3,31 @@ import axios from 'axios';
 
 const PDFViewer = ({ pdfUrl }) => {
   const [pdf, setPdf] = useState('');
-  const [isLoaded, setIsLoaded] = useState(false);
 
-  console.log("manggil:", pdfUrl);
+  const fetchPdf = async () => {
+    try {
+      const response = await axios.get(`/api/kak/file/${pdfUrl}`, { responseType: 'blob' });
+      const pdfBlob = URL.createObjectURL(new Blob([response.data], { type: 'application/pdf' }));
+      setPdf(pdfBlob);
+    } catch (error) {
+      console.error('Error fetching the PDF: ', error);
+    }
+  };
 
   useEffect(() => {
-    if (!isLoaded && pdfUrl) {
-      axios.get(`/api/kak/file/${pdfUrl}`, { responseType: 'blob' })
-        .then(response => {
-          const pdfBlob = URL.createObjectURL(new Blob([response.data], { type: 'application/pdf' }));
-          setPdf(pdfBlob);
-          setIsLoaded(true); // Setelah selesai, tandai bahwa telah dimuat
-        })
-        .catch(error => {
-          console.error('Error fetching the PDF: ', error);
-        });
-    }
-  }, [pdfUrl, isLoaded]);
+    const openModal = async () => {
+      if (pdfUrl) {
+        await fetchPdf();
+      }
+    };
+    openModal();
+  }, [pdfUrl]);
+
+  useEffect(() => {
+    return () => {
+      setPdf('');
+    };
+  }, [pdfUrl]);
 
   return (
     <div>
